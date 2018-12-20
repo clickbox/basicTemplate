c90e1ce245a068c786430f7e691672a5fd6a2117
Ezenwa Isiogu <yo@ezenwa.com>
2018-12-20 14:28:16 -0500

    init commit

---
+++ js/sceneMain.js
@@ -0,0 +1,21 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload() {
+    	// load images or sounds
+    }
+
+    create() {
+        // define our objects
+        console.log("Ready!");
+    }
+
+    update() {
+        // constant running loop
+    }
+
+    customFunctions() {
+        
+    }
+}
